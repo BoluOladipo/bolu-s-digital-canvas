4e95ff41d1b990eb0f748d935aaa7d7f3b81b25c
Bolu Oladipo <boluemmanuel071@gmail.com>
2026-01-31 13:12:14 +0100

    Add IntroLoader component with basic structure

---
+++ src/components/IntroLoader.jsx
@@ -0,0 +1,11 @@
+import "./introLoader.css";
+
+const IntroLoader = () => {
+  return (
+    <div className="intro-container">
+      <h1 className="intro-text">BoluOladipoCode</h1>
+    </div>
+  );
+};
+
+export default IntroLoader;
